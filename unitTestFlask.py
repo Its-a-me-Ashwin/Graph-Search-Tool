e02bc157ac4a65f7f037fad4427d561fee107494
Ashwin R Bharadwaj <ashwinrb7799@gmail.com>
2020-05-29 18:19:33 +0530

    Unit testing has been added

--- unitTestFlask.py
+++ unitTestFlask.py
@@ -1,5 +1,6 @@
 from flaskSever import app
 import unittest
+import json
 
 class FlaskTestCase (unittest.TestCase):
 
@@ -34,14 +35,81 @@ class FlaskTestCase (unittest.TestCase):
         response = tester.post("/api/v1/info/ucs",content_type = "json/text")
         self.assertEqual(response.status_code,200)
 
-
-    def testPathASR():
+    def testPathASR(self):
         tester = app.test_client(self)
-        response = tester.post("/api/v1/asr",
-                                content_type = "json/text",
-                                
+        response = tester.post("/api/v1/asr",   
+                                content_type = "application/json",
+                                data = json.dumps(dict())
+                                )
+        self.assertEqual(response.status_code,400)
+        return
+
+    def testPathDFS(self):
+        tester = app.test_client(self)
+        response = tester.post("/api/v1/dfs",   
+                                content_type = "application/json",
+                                data = json.dumps(dict())
+                                )
+        self.assertEqual(response.status_code,404)
+        return
+
+    def testPathBFS(self):
+        tester = app.test_client(self)
+        response = tester.post("/api/v1/bfs",   
+                                content_type = "application/json",
+                                data = json.dumps(dict())
+                                )
+        self.assertEqual(response.status_code,400)
+        return
+
+    def testPathASR(self):
+        tester = app.test_client(self)
+        response = tester.post("/api/v1/asr",   
+                                content_type = "application/json",
+                                data = json.dumps(dict())
+                                )
+        self.assertEqual(response.status_code,400)
+        return
+    def testCorrect(self):
+        tester = app.test_client(self)
+        data = {
+            "nodes" : ['1','2'],
+            "edges" : [["1","2"]],
+            "weights" : [0],
+            "src" : "1",
+            "dst" : "2"
+        }
+        response = tester.post("/api/v1/bfs",   
+                                content_type = "application/json",
+                                data = json.dumps(data)
                                 )
         self.assertEqual(response.status_code,200)
+        response = tester.post("/api/v1/Uniform",   
+                                content_type = "application/json",
+                                data = json.dumps(data)
+                                )
+        self.assertEqual(response.status_code,200)
+        return
+    def testErroes(self):
+        tester = app.test_client(self)
+        data = {
+            "node" : ['1','2'],
+            "edges" : [],
+            "weights" : [],
+            "src" : "1",
+            "dst" : "3"
+        }
+        response = tester.post("/api/v1/bfs",   
+                                content_type = "application/json",
+                                data = json.dumps(data)
+                                )
+        self.assertEqual(response.status_code,400)
+        response = tester.post("/api/v1/Uniform",   
+                                content_type = "application/json",
+                                data = json.dumps(data)
+                                )
+        self.assertEqual(response.status_code,400)
+        return
 
 if __name__ == "__main__":
     unittest.main()
